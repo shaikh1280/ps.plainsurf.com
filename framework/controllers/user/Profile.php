03dadcedee5335f49f17f4843420c475d50fcaee
Yusuf Shaikh <yusuf@plainsurf.com>
2018-02-01 23:35:54 +0530

    updated user level class

---
+++ framework/controllers/user/Profile.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Profile 
+ *
+ * @author farhat
+ */
+class Profile extends CI_Controller {
+    //put your code here
+    public function index() {
+        echo 'user profile section';     
+    }
+}
