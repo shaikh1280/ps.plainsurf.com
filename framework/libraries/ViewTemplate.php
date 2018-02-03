fd882fef326f6131f64c423b376f65bcef2f0188
Yusuf Shaikh <yusuf@plainsurf.com>
2018-02-03 23:15:57 +0530

    adding missing file

---
+++ framework/libraries/ViewTemplate.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of View
+ *
+ * @author farhat
+ */
+class ViewTemplate {
+    private $controller;
+    private $fileName; 
+    private $data;
+    private $extra;
+    function __construct($viewConfig) {
+        $this->controller  = &get_instance(); 
+        $this->fileName = $viewConfig['fileName'];
+        $this->data = $viewConfig['data'];
+        $this->extra = isset($viewConfig['extra'])? $viewConfig['extra'] : array();
+    }
+
+    public function render() {
+        $this->controller->load->view('index', $this->data);
+    }
+}
