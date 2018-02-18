5c4a17902dea85dca95e555a76d7c30f434cae0b
Faizan Shaikh <faizan@plainsurf.com>
2018-02-18 15:34:05 +0530

    View issue resolved

--- framework/views/user/setting.php
+++ framework/views/user/setting.php
@@ -1,3 +1,28 @@
 <?php
-    echo "Hello from profile";
+
 ?>
+<style>
+    table, th, td{
+        border: 1px solid black;
+        border-collapse: collapse;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        padding: 20px;
+        width: 50%;
+    }
+</style>
+<table align="center">
+    <tr>
+        <th colspan="2"style="text-align: center; padding: 20px">Recently used devices</th>
+    </tr>
+    <?php
+            
+        foreach ($result as $r){
+            echo '<tr>';
+            echo '<td style="padding:20px">'.$r->system_type."</td>";
+            echo '<td style="padding:20px">'.$r->system_name."</td>";
+            echo '</tr>';
+        }
+    
+    ?>
+</table>
